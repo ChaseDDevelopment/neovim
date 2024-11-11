9def77ea6909988e9db47b6226ae3ec05607d082
ChaseDDevelopment <Chase.D.Dubauskas@protonmail.com>
2024-11-10 23:56:03 -0700

    Move vim options to seperate file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,10 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.wo.number = true
-vim.wo.relativenumber = true
+require("config.vim-options")
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
