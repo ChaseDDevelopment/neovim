2579a5669086fed5def7526a259a09c9c7acc0f0
ChaseDDevelopment <Chase.D.Dubauskas@protonmail.com>
2024-11-10 23:19:15 -0700

    Cleanup lazy.lua and seperate out plugins to specs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,15 +40,3 @@ require("lazy").setup({
 vim.cmd.colorscheme "catppuccin"
 
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-local config = require('nvim-treesitter.configs')
-config.setup({
-	ensure_installed = {"lua", "markdown", "python", "javascript", "c_sharp"},
-	highlight = true,
-	indent = true	
-})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,21 @@
 return {
-	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"}
+  'nvim-treesitter/nvim-treesitter',
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      -- List of parsers to install
+      ensure_installed = { "lua", "markdown", "python", "javascript", "c_sharp" },
+
+      -- Enable syntax highlighting
+      highlight = {
+        enable = true,              -- false will disable the whole extension
+        additional_vim_regex_highlighting = false,
+      },
+
+      -- Enable indentation based on treesitter
+      indent = {
+        enable = true,
+      },
+    }
+  end
 }
