7bb347d3463e7207ed1a7c39a278988cfde8c578
ChaseDDevelopment <Chase.D.Dubauskas@protonmail.com>
2024-11-11 13:44:47 -0700

    Add Code Snippets

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,7 +3,6 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	lazy = false,
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.startify")
