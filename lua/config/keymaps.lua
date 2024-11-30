b9366969c06d5245c10e0a35e471c8f814155baf
ChaseDDevelopment <Chase.D.Dubauskas@protonmail.com>
2024-11-29 17:13:01 -0700

    Update LazyVim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume telescope" }
+)
+
+-- Visual mode: Move selected lines up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+-- Normal mode: Half-page jump with cursor centered
+vim.keymap.set("n", "<C-j>", "<C-d>zz", { desc = "Scroll half-page down and center" })
+vim.keymap.set("n", "<C-k>", "<C-u>zz", { desc = "Scroll half-page up and center" })
