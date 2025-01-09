44b57868aa40f95791090e794823f8cbc409f346
ChaseDDevelopment <Chase.D.Dubauskas@protonmail.com>
2025-01-09 13:43:32 -0700

    Update conform config.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,48 @@
 return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
+    -- Define filetypes supported by Prettier
+    local supported = {
+      "javascript",
+      "typescript",
+      "javascriptreact",
+      "typescriptreact",
+      "vue",
+      "svelte",
+      "css",
+      "scss",
+      "html",
+      "json",
+      "yaml",
+    }
+
+    -- Ensure `opts.formatters_by_ft` exists
     opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    -- Add Prettier for its supported filetypes
     for _, ft in ipairs(supported) do
       opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
       table.insert(opts.formatters_by_ft[ft], "prettier")
     end
+
+    -- Add Ruff for Python
+    opts.formatters_by_ft.python = opts.formatters_by_ft.python or {}
+    table.insert(opts.formatters_by_ft.python, "ruff")
+
+    -- Ensure `opts.formatters` exists
     opts.formatters = opts.formatters or {}
+
+    -- Configure Prettier
     opts.formatters.prettier = {
       condition = function(_, ctx)
+        local M = require("conform.util")
         return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
       end,
     }
-    opts.formatters_by_ft.python = opts.formatters_by_ft.python or {}
-    table.insert(opts.formatters_by_ft.python, "black")
-    opts.formatters.black = {
-      prepend_args = { "--line-length", "79" },
+
+    -- Configure Ruff with line length of 79
+    opts.formatters.ruff = {
+      prepend_args = { "--config", "line-length=79" },
     }
 
     return opts
